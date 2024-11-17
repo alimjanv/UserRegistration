70fecf78bc4ea6f015c7ca8d9de428c0cc6215e6
maralimjanv <umarolimjonov2009@gmail.com>
2024-11-18 00:51:55 +0500

    tru

--- user_registry/apps.py
+++ user_registry/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class UserRegistryConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'user_regist'
+    name = 'user_registry'
